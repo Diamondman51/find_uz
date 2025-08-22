f7649a753dc47f96ab7d101164c84608c995fd88
Diamondman51 <zshavkatov51@gmail.com>
2025-08-22 16:51:49 +0500

    create term update

--- core/settings.py
+++ core/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = secrets.token_hex(32)
-SECRET_KEY = 'secrets.token_hex(32)'
+SECRET_KEY = secrets.token_hex(32)
+# SECRET_KEY = 'secrets.token_hex(32)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
