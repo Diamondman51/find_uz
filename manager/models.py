e693c3e631fd48115bbb7c9539c58c0f3aaa7c77
Diamondman51 <zshavkatov51@gmail.com>
2025-09-04 15:21:27 +0500

    added proc name

--- manager/models.py
+++ manager/models.py
@@ -6,4 +6,5 @@ class App(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     start = models.TimeField()
     end = models.TimeField()
+    proc_name = models.CharField(max_length=100, blank=True, null=True)
     for_whom = models.CharField(max_length=100, blank=False, null=False)
