47481b4275e0b28d2d5a12f6aea18b5037115d4a
Diamondman51 <zshavkatov51@gmail.com>
2025-09-04 23:39:59 +0500

    excepts added

--- manager/models.py
+++ manager/models.py
@@ -7,4 +7,5 @@ class App(models.Model):
     start = models.TimeField()
     end = models.TimeField()
     proc_name = models.CharField(max_length=100, blank=True, null=True)
+    excepts = models.JSONField(default=list)
     for_whom = models.CharField(max_length=100, blank=False, null=False)
